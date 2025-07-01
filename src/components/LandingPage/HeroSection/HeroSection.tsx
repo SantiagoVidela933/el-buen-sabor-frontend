016ee0bcfc279b53e60b8fa7b47aff42c9516bf6
bruno2803 <104646736+bruno2803@users.noreply.github.com>
2025-07-01 13:16:33 -0300

    cambio herosection

--- src/components/LandingPage/HeroSection/HeroSection.tsx
+++ src/components/LandingPage/HeroSection/HeroSection.tsx
@@ -10,13 +10,13 @@ const HeroSection = () => {
       <div className={styles.textContainer}>
         <h2>Todo lo que necesitás, ¡te lo llevamos!</h2>
         <p>
-          En el Buen Sabor podés dejar volar tu imaginación porque ahora todo lo que quieras lo llevamos directo desde tu cabeza a donde estés, ¡en minutos!
+          En Buen Sabor podés dejar volar tu imaginación, porque todo lo que se te antoje lo llevamos directo desde la cocina hasta tu mesa, ¡en minutos!
         </p>
         <p>
-          ¿Lo querés? ¡Lo tenés! Pedí a los mejores restaurantes, hacé el pedido del súper, comprá la comida para tu mascota, la bebida para los amigos, pedí lo que necesites de la farmacia ¡y mucho más!
+          ¿Lo querés? ¡Lo tenés! Pedí tus platos favoritos de los mejores locales gastronómicos, hacé tu pedido fácil y rápido, y disfrutá de una experiencia de sabor sin moverte de donde estés.
         </p>
         <p>
-          En simples pasos podés tener lo que quieras directamente en tu puerta: descubrí, pedí y recibí a domicilio con el Buen Sabor.
+          En simples pasos podés tener lo que quieras directamente en tu puerta: descubrí, pedí y recibí comida deliciosa con Buen Sabor.
         </p>
       </div>
     </section>
